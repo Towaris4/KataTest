fef646b272f7246624db41a0227277fc247f8455
Towaris4 <ivan.tarasov2488@gmail.com>
2023-12-14 12:04:27 +0300

    Test kata

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        final String MATH_DIVIDE = "/";
+        final String MATH_MULTIPLY = "*";
+        final String MATH_MINUS = "-";
+        final String MATH_PLUS = "+";
+        Scanner in = new Scanner(System.in);
+        String mathExample = in.nextLine();
+        System.out.print(mathExample);
+        }
+    public static boolean checkValid(String mathExample) {
+        indexOf()
+        lastIndexOf()
+    }
+    public static String detectSeparator() {
+        indexOf();
+        lastIndexOf();
+    }
+    }
