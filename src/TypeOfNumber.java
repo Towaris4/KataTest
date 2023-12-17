3d177a9a3a7582c39db0c15bcaedfbc60ec3a36d
Towaris4 <ivan.tarasov2488@gmail.com>
2023-12-17 18:15:32 +0300

    Доделать

---
+++ src/TypeOfNumber.java
@@ -0,0 +1,94 @@
+public class TypeOfNumber {
+    int numValue = 0;
+    String Str_num_A = null;
+    boolean isRim;
+    public void getType() {
+        switch (Str_num_A) {
+            case "i":
+                numValue = 1;
+                isRim = true;
+                break;
+            case "ii":
+                numValue = 2;
+                isRim = true;
+                break;
+            case "iii":
+                numValue = 3;
+                isRim = true;
+                break;
+            case "iv":
+                numValue = 4;
+                isRim = true;
+                break;
+            case "v":
+                numValue = 5;
+                isRim = true;
+                break;
+            case "vi":
+                numValue = 6;
+                isRim = true;
+                break;
+            case "vii":
+                numValue = 7;
+                isRim = true;
+                break;
+            case "viii":
+                numValue = 8;
+                isRim = true;
+                break;
+            case "ix":
+                numValue = 9;
+                isRim = true;
+                break;
+            case "x":
+                numValue = 10;
+                isRim = true;
+                break;
+            case "0":
+                numValue = 0;
+                isRim = false;
+                break;
+            case "1":
+                numValue = 1;
+                isRim = false;
+                break;
+            case "2":
+                numValue = 2;
+                isRim = false;
+                break;
+            case "3":
+                numValue = 3;
+                isRim = false;
+                break;
+            case "4":
+                numValue = 4;
+                isRim = false;
+                break;
+            case "5":
+                numValue = 5;
+                isRim = false;
+                break;
+            case "6":
+                numValue = 6;
+                isRim = false;
+                break;
+            case "7":
+                numValue = 7;
+                isRim = false;
+                break;
+            case "8":
+                numValue = 8;
+                isRim = false;
+                break;
+            case "9":
+                numValue = 9;
+                isRim = false;
+                break;
+            case "10":
+                numValue = 10;
+                isRim = false;
+                break;
+
+        }
+    }
+}
